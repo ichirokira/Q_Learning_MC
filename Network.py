9dd181d894feea1ce62fdbb3e34eaf0fad10aa63
thuydung-icthust <thuydungnguyen.ictk63@gmail.com>
2020-11-05 10:25:00 +0700

    add file

--- Network.py
+++ Network.py
@@ -72,8 +72,8 @@ class Network:
         energy_log.close()
 
     def simulate_max_time(self, optimizer, max_time=10000, file_name="log/information_log.csv"):
-        file_name = "./log/Q_learning2nd/thaydoisonode/result_5_intervals/alpha0.3/information_log" + str(self.index) +".csv"
-        filenametxt = "./log/Q_learning2nd/thaydoisonode/result_5_intervals/alpha0.3/networkinfor" + str(self.index) +".txt"
+        file_name = "./log/Q_learning2nd/thaydoisonode/result_5_intervals/alpha0.4/information_log" + str(self.index) +".csv"
+        filenametxt = "./log/Q_learning2nd/thaydoisonode/result_5_intervals/alpha0.4/networkinfor" + str(self.index) +".txt"
         information_log = open(file_name, "a+")
         writer = csv.DictWriter(information_log, fieldnames=["time", "nb dead", "nb package"])
         writer.writeheader()
